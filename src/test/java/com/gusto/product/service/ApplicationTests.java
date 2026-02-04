7a5332f17944a8be7058e6f58acd60fa76c742af
Gavindu Sajeendra <gavindu1995@gmail.com>
2026-02-04 15:28:39 +0530

    updated product service

--- src/test/java/com/gusto/product/service/ApplicationTests.java
+++ src/test/java/com/gusto/product/service/ApplicationTests.java
@@ -1,13 +1,56 @@
 package com.gusto.product.service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:7.0.5"));
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+
+	}
+	static {
+		mongoDBContainer.start();
+	}
 
 	@Test
 	void contextLoads() {
+		String requestBody = """
+{
+  "name": "iPhone 13",
+  "description": "iPhone 13 is a smartphone from Apple",
+  "price": 1000
+}
+""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name",Matchers.equalTo("iPhone 13"))
+				.body("description", Matchers.equalTo("iPhone 13 is a smartphone from Apple"))
+				.body("price", Matchers.equalTo(1000));
+
 	}
 
 }
